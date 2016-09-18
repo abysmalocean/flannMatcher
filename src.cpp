cfac55104be532f62e199e03191df451cc07ee18
Dongwei <wdw828@gmail.com>
2016-09-18 02:21:57 -0400

    loop to iterate all source files

--- src.cpp
+++ src.cpp
@@ -6,6 +6,14 @@
 
 #include "opencv2/opencv_modules.hpp"
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #ifndef HAVE_OPENCV_NONFREE
 
@@ -23,6 +31,7 @@ int main(int, char**)
 # include "opencv2/nonfree/features2d.hpp"
 
 using namespace cv;
+using namespace std;
 
 void readme();
 
@@ -32,89 +41,115 @@ void readme();
  */
 int main( int argc, char** argv )
 {
-	if( argc != 3 ){
-		readme(); return -1;
-	}
-
-	Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-	Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
-
-	if( !img_1.data || !img_2.data ){
-		printf(" --(!) Error reading images \n");
+	if( argc != 2 ){
+		readme();
 		return -1;
 	}
 
-	//-- Step 1: Detect the keypoints using SURF Detector
-	int minHessian = 400;
-	SurfFeatureDetector detector( minHessian );
-	std::vector<KeyPoint> keypoints_1, keypoints_2;
-	detector.detect( img_1, keypoints_1 );
-	detector.detect( img_2, keypoints_2 );
+	string dir("/home/wangd7/Desktop/flannMatcher/image/source");
+	cout<<dir<<endl;
+	string filepath;
+	DIR *dp;
+	struct dirent *dirp;
+	//cout<<"Input the file directory!"<<endl;
+	//cin>>dir;
 
-	//-- Step 2: Calculate descriptors (feature vectors)
-	SurfDescriptorExtractor extractor;
-	Mat descriptors_1, descriptors_2;
-	extractor.compute( img_1, keypoints_1, descriptors_1 );
-	extractor.compute( img_2, keypoints_2, descriptors_2 );
+	dp = opendir(dir.c_str());
 
-	//-- Step 3: Matching descriptor vectors using FLANN matcher
-	FlannBasedMatcher matcher;
-	std::vector< DMatch > matches;
-	matcher.match( descriptors_1, descriptors_2, matches );
-
-	double max_dist = 0; double min_dist = 100;
-
-	//-- Quick calculation of max and min distances between keypoints
-	for( int i = 0; i < descriptors_1.rows; i++ )
-	{
-		double dist = matches[i].distance;
-		if( dist < min_dist )
-			min_dist = dist;
-		if( dist > max_dist )
-			max_dist = dist;
+	if(dp == NULL){
+		cout<<"Error opening "<<dir<<endl;
+		return 0;
 	}
 
-	printf("-- Max dist : %f \n", max_dist );
-	printf("-- Min dist : %f \n", min_dist );
+	Mat img_2 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
+	while((dirp = readdir(dp))){
+		filepath = dir + "/" + dirp->d_name;
+				if(strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0 ){
+			continue;
+		}
+		else{
+			cout<<"*****"<<filepath<<"*****"<<endl;
+			Mat img_1 = imread( filepath.c_str(), CV_LOAD_IMAGE_GRAYSCALE );
 
-	//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-	//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-	//-- small)
-	//-- PS.- radiusMatch can also be used here.
-	std::vector< DMatch > good_matches;
+			if( !img_1.data ){
+				printf(" --(!) Error reading images \n");
+				return -1;
+			}
 
-	for( int i = 0; i < descriptors_1.rows; i++ )
-	{
-		if( matches[i].distance <= max(2*min_dist, 0.02) ){
-			good_matches.push_back( matches[i]);
+			//-- Step 1: Detect the keypoints using SURF Detector
+			int minHessian = 400;
+			SurfFeatureDetector detector( minHessian );
+			std::vector<KeyPoint> keypoints_1, keypoints_2;
+			detector.detect( img_1, keypoints_1 );
+			detector.detect( img_2, keypoints_2 );
+
+			//-- Step 2: Calculate descriptors (feature vectors)
+			SurfDescriptorExtractor extractor;
+			Mat descriptors_1, descriptors_2;
+			extractor.compute( img_1, keypoints_1, descriptors_1 );
+			extractor.compute( img_2, keypoints_2, descriptors_2 );
+
+			//-- Step 3: Matching descriptor vectors using FLANN matcher
+			FlannBasedMatcher matcher;
+			std::vector< DMatch > matches;
+			matcher.match( descriptors_1, descriptors_2, matches );
+
+			double max_dist = 0; double min_dist = 100;
+
+			//-- Quick calculation of max and min distances between keypoints
+			for( int i = 0; i < descriptors_1.rows; i++ )
+			{
+				double dist = matches[i].distance;
+				if( dist < min_dist )
+					min_dist = dist;
+				if( dist > max_dist )
+					max_dist = dist;
+			}
+
+		//	printf("-- Max dist : %f \n", max_dist );
+		//	printf("-- Min dist : %f \n", min_dist );
+
+			//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+			//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+			//-- small)
+			//-- PS.- radiusMatch can also be used here.
+			std::vector< DMatch > good_matches;
+
+			for( int i = 0; i < descriptors_1.rows; i++ )
+			{
+				if( matches[i].distance <= max(2*min_dist, 0.02) ){
+					good_matches.push_back( matches[i]);
+				}
+			}
+
+			//-- Draw only "good" matches
+			Mat img_matches;
+			drawMatches( img_1, keypoints_1, img_2, keypoints_2,
+					good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+			//-- Show detected matches
+			imshow( "Good Matches", img_matches );
+
+			double sum = 0;
+			for( int i = 0; i < (int)good_matches.size(); i++ ){
+				sum += good_matches[i].distance;
+				// printf( "-- Good Match [%d] Keypoint 1: %4d  -- Keypoint 2: %4d  good distance: %.4f\n",
+				//         i, good_matches[i].queryIdx, good_matches[i].trainIdx, good_matches[i].distance );
+			}
+			printf("average value is %.4f\n\n", sum/good_matches.size());
+		//	waitKey(0);
 		}
 	}
 
-	//-- Draw only "good" matches
-	Mat img_matches;
-	drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-			good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-			vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-	//-- Show detected matches
-	imshow( "Good Matches", img_matches );
-
-	double sum = 0;
-	for( int i = 0; i < (int)good_matches.size(); i++ ){
-		sum += good_matches[i].distance;
-		printf( "-- Good Match [%d] Keypoint 1: %4d  -- Keypoint 2: %4d  good distance: %.4f\n",
-				i, good_matches[i].queryIdx, good_matches[i].trainIdx, good_matches[i].distance );
-	}
-	printf("average value is %.4f\n", sum/good_matches.size());
-//	waitKey(0);
-	return 0;
+	closedir(dp);
 }
 
 /**
  * @function readme
  */
 void readme(){
-	printf(" Usage: ./SURF_FlannMatcher <img1> <img2>\n");
+	cout<<" Usage: ./SURF_FlannMatcher <img1> <img2>"<<endl;
 }
 
 #endif
