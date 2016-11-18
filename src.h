1d974bd8bafc124817345b7735e694cb43b5463d
liangxu <liangxuav@gmail.com>
2016-11-18 14:14:33 -0500

    fix

--- src.h
+++ src.h
@@ -26,7 +26,7 @@
 # include "opencv2/core/core.hpp"
 # include "opencv2/features2d/features2d.hpp"
 # include "opencv2/highgui/highgui.hpp"
-# include "features2d.hpp"
+# include "nonefree/features2d.hpp"
 
 using namespace cv;
 using namespace std;
